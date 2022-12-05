cc1bcd3d245c752e96faf0b2d35b981cfab6a2b0
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-12-05 15:44:26 +0400

    const colors = tokens(theme.palette.mode)//прохожу в цвета во все и в палитру и в режим

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import {ColorModeContext, useMode} from '../theme'
 import { CssBaseline, ThemeProvider } from "@mui/material";//сбросит и проведёт
+import Topbar from './scenes/dashboard/global/Topbar';
+
 
 function App() {
   const [theme, colorMode]= useMode();
@@ -9,7 +11,11 @@ function App() {
   return (
     <ColorModeContext.Provider value={colorMode}>{/*способ настроить наш цвет */}
     <ThemeProvider theme={theme}>{/*поставщик чтобы пользовательский интерфейс имел к нему доступ */}
+    <CssBaseline />{/*сбросит css по умолчанию */}
     <div className="App">
+    <main className="content">{/*основной раздел */}
+    <Topbar/>{/*верхняя панель */}
+    </main>
     </div>
     </ThemeProvider>
     </ColorModeContext.Provider>
--- src/scenes/dashboard/global/Topbar.jsx
+++ src/scenes/dashboard/global/Topbar.jsx
@@ -1,8 +1,19 @@
-//верхняя панель
-
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
 import React from 'react'
+import {ColorModeContext, tokens} from '../../../../theme';//цветовой режим
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";//светлый режим
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 
+//верхняя панель
 const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode)//прохожу в цвета во все и в палитру и в режим
   return (
     <div>
       
@@ -10,4 +21,4 @@ const Topbar = () => {
   )
 }
 
-export default Topbar
+export default Topbar;
